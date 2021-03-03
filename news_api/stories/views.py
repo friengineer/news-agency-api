e6bbce6acdf5cbc31e8962c2dc63e57fee2514eb
Reece Dudley <ll16rdjd@leeds.ac.uk>
2021-03-03 21:42:11 +0000

    Implemented logging in and out using Django authentication, need to test

--- news_api/stories/views.py
+++ news_api/stories/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login
 
 @csrf_exempt
 def Login(request):
-    return HttpResponse('Login not implemented')
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+
+        return HttpResponse('You are logged in, welcome back!', content_type='text/plain')
+    else:
+        return HttpResponse('Failed login. The credentials supplied are incorrect', content_type='text/plain', status=503)
 
 @csrf_exempt
 def Logout(request):
-    return HttpResponse('Logout not implemented')
+    try:
+        logout(request)
+
+        return HttpResponse('You have successfully logged out, goodbye.', content_type='text/plain')
+    except:
+        return HttpResponse('Logout failed and you have not been logged out', content_type='text/plain', status=503)
 
 @csrf_exempt
 def CreateStory(request):
