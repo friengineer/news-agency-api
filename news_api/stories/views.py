ab9c4cf46c3e7eb53e8b439b2a301ed3e4c70f52
Reece Dudley <ll16rdjd@leeds.ac.uk>
2021-03-06 22:50:40 +0000

    Imported logout function and login and logout function work now. Implemented framework for post and delete story. Created requirements file to speed up installing environment.

--- news_api/stories/views.py
+++ news_api/stories/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 @csrf_exempt
 def Login(request):
@@ -14,24 +14,42 @@ def Login(request):
 
         return HttpResponse('You are logged in, welcome back!', content_type='text/plain')
     else:
-        return HttpResponse('Failed login. The credentials supplied are incorrect', content_type='text/plain', status=503)
+        return HttpResponse('Failed login. The credentials supplied are incorrect.', content_type='text/plain', status=503)
 
 @csrf_exempt
 def Logout(request):
     try:
         logout(request)
 
-        return HttpResponse('You have successfully logged out, goodbye.', content_type='text/plain')
+        response = HttpResponse('You have successfully logged out, goodbye.', content_type='text/plain')
     except:
-        return HttpResponse('Logout failed and you have not been logged out', content_type='text/plain', status=503)
+        response = HttpResponse('Logout failed and you have not been logged out.', content_type='text/plain', status=503)
+
+    return response
 
 @csrf_exempt
 def CreateStory(request):
-    return HttpResponse('Create story not implemented')
+    if not request.user.is_authenticated:
+        return HttpResponse('Unable to post story because user is not logged in.', content_type='text/plain', status=503)
+
+    try:
+        response = HttpResponse('Your story has been posted.', content_type='text/plain', status=201)
+    except:
+        response = HttpResponse('An error occurred and your story has not been posted.', content_type='text/plain', status=503)
+
+    return response
 
 def ListStories(request):
     return HttpResponse('List stories not implemented')
 
 @csrf_exempt
 def DeleteStory(request):
-    return HttpResponse('Delete story not implemented')
+    if not request.user.is_authenticated:
+        return HttpResponse('Unable to delete story because user is not logged in.', content_type='text/plain', status=503)
+
+    try:
+        response = HttpResponse('The story has been deleted.', content_type='text/plain', status=201)
+    except:
+        response = HttpResponse('An error occurred and the story has not been deleted.', content_type='text/plain', status=503)
+
+    return response
